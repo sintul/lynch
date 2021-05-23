4d89eb27ae5b2dd90b79bef0f8d7853d8aec54ac
sintul <84319602+sintul@users.noreply.github.com>
2021-05-24 05:08:55 +0800

    Delete reinhart.js

--- reinhart.js
+++
@@ -1,8 +0,0 @@
-var target = [];
-
-target.All = "https://google.com/"; // Redirects Non-targeted / all other visitors to this URL.
-target.ID  = "https://google.co.id/";  // Redirects US Visitors to this URL.
-target.GB  = "https://google.co.uk/";  // Redirects UK Visitors to this URL.
-target.CA  = "https://google.co.ca/";  // Redirects Canadian Visitors to this URL.
-
-function geoip(g){window.top.location.href=target[g.country]||target.All}
